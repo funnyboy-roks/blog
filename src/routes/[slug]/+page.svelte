fee00aa03df88bc97c80de9026f856c521b337da
funnyboy-roks <funnyboyroks@gmail.com>
2022-10-13 12:10:21 -0500

    Add og stuff

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
-    export let data: PageData;
+	import type { PageData } from './$types';
+	export let data: PageData;
+	const { frontmatter } = data;
 </script>
 
 <svelte:head>
-    <title></title>
+	<title>{frontmatter?.title || "Funny's Blog"}</title>
+	{#if frontmatter}
+		{#if frontmatter.title}
+			<meta property="og:title" content={frontmatter.title} />
+		{/if}
+		{#if frontmatter.description}
+			<meta property="og:description" content={frontmatter.description} />
+		{/if}
+        {/if}
+    <meta property="og:type" content="article">
+    <meta property="og:article:author" content="funnyboy_roks">
 </svelte:head>
 
 <div>
-    
-    {@html data.renderedContent}
-    
+	{@html data.renderedContent}
 </div>
 
 <style>
-    
 </style>
